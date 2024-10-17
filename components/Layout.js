cc2cad2fca74220021285035bfdea3fea72b5748
MrHeckerCat <129594453+MrHeckerCat@users.noreply.github.com>
2024-10-17 18:56:15 +0200

    Update Layout.js

--- components/Layout.js
+++ components/Layout.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
-import styles from '/styles/layout.module.css';
+import styles from '../styles/layout.module.css';
 
 export default function Layout({ children }) {
   return (
