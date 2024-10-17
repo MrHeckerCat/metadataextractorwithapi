742bf2934d308115cd468bdf1fe8a42fe2e130c1
MrHeckerCat <129594453+MrHeckerCat@users.noreply.github.com>
2024-10-17 19:16:55 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,25 +1,9 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ['cxubggguaz3oakrz.public.blob.vercel-storage.com'], // Replace with your actual upload domain
-  },
-  async headers() {
-    return [
-      {
-        source: '/api/:path*',
-        headers: [
-          { key: 'Access-Control-Allow-Credentials', value: 'true' },
-          { key: 'Access-Control-Allow-Origin', value: '*' },
-          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
-          { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version' },
-        ],
-      },
-    ];
-  },
-};
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['cxubggguaz3oakrz.public.blob.vercel-storage.com'],
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     config.resolve.fallback = { fs: false, path: false };
     return config;
