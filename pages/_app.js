f8e3aa29022dc84fd4f01b1e542ec079afe4ceb3
MrHeckerCat <129594453+MrHeckerCat@users.noreply.github.com>
2025-02-03 10:55:31 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,8 @@
 import Layout from '../components/Layout.js';
 import '../styles/globals.css';
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
+import * as gtag from '../lib/gtag'
 
 function MyApp({ Component, pageProps }) {
   return (
@@ -7,6 +10,21 @@ function MyApp({ Component, pageProps }) {
       <Component {...pageProps} />
     </Layout>
   );
+}
+function MyApp({ Component, pageProps }) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
+  return <Component {...pageProps} />
 }
 
 export default MyApp;
