d99bd367204bbc07e51e4e4c95f4df6d118754ec
MrHeckerCat <129594453+MrHeckerCat@users.noreply.github.com>
2025-02-19 08:28:29 +0100

    Create FeedbackMessage.js

---
+++ components/FeedbackMessage.js
@@ -0,0 +1,10 @@
+const FeedbackMessage = () => {
+  return (
+    <div className="feedback-message">
+      <p>Was this metadata extraction helpful? We love to hear your feedback!</p>
+      <p>Send us your thoughts at <a href="mailto:info@imagedataextract.com">info@imagedataextract.com</a></p>
+    </div>
+  );
+};
+
+export default FeedbackMessage;
